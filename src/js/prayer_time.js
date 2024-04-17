add57ebca591483679ac0e64ed56d6aeacac445c
bilhasry <bilhasry@gmail.com>
2024-04-18 01:27:09 +0800

    add localstorage for api call

--- src/js/prayer_time.js
+++ src/js/prayer_time.js
@@ -26,7 +26,6 @@ async function getPrayerTime() {
 
 function formatHijriDate(date) {
     let hijri = date.split('-');
-    console.log(date);
     return `${hijri[2]} ${HIJRI_MONTHS[parseInt(hijri[1])-1]} ${hijri[0]}`; 
 }
 
@@ -34,22 +33,71 @@ function formatGregorianDate(date) {
     let gregorian = new Date(date);
     return `${gregorian.getDate()} ${GREGORIAN_MONTHS[gregorian.getMonth()]} ${gregorian.getFullYear()} `; 
 }
+
+function isStorageExist()
+{
+    return localStorage.getItem('prayer_times') ?? false;
+}
+
+function isToday(prayerDate)
+{
+    let todayDate = new Date();
+    let storageDate = new Date(prayerDate);
+    
+    return todayDate.toDateString() === storageDate.toDateString();
+}
+
+function getPrayerFromStorage()
+{
+    return JSON.parse(localStorage.getItem('prayer_times'));
+}
+
 window.onload = function() {
-    let prayers = getPrayerTime();
-    prayers.then(data => {
-        let prayerTime = data.prayerTime[0];
-        
-        for (const [key, value] of Object.entries(prayerTime)) {
-            if (PRAYER_TIMES.includes(key)) {
-                document.querySelector(`.${key} > h3`).innerHTML = PRAYER_TIMES_TRANSLATE[key];
-                document.querySelector(`.${key} > div`).innerHTML = value.substring(0,5);
-            }
-            if (key === 'date') {
-                document.querySelector('.gregorian').innerHTML = formatGregorianDate(value);
-            }
-            if (key === 'hijri') {
-                document.querySelector('.hijri').innerHTML = formatHijriDate(value);
-            }
+    let callApi = true;
+    let prayerStorage = {};
+    if (isStorageExist()) {
+        prayerStorage = getPrayerFromStorage();
+        if (isToday(prayerStorage.serverTime)) {
+            callApi = false;
         }
-    })   
+    }
+    
+    if (callApi) {
+        console.log(callApi);
+        let prayers = getPrayerTime();
+        prayers.then(data => {
+            localStorage.setItem('prayer_times', JSON.stringify(data));
+            let prayerTime = data.prayerTime[0];
+            
+            for (const [key, value] of Object.entries(prayerTime)) {
+                if (PRAYER_TIMES.includes(key)) {
+                    document.querySelector(`.${key} > h3`).innerHTML = PRAYER_TIMES_TRANSLATE[key];
+                    document.querySelector(`.${key} > div`).innerHTML = value.substring(0,5);
+                }
+                if (key === 'date') {
+                    document.querySelector('.gregorian').innerHTML = formatGregorianDate(value);
+                }
+                if (key === 'hijri') {
+                    document.querySelector('.hijri').innerHTML = formatHijriDate(value);
+                }
+            }
+        });
+    } else {
+        let prayers = prayerStorage;
+
+        let prayerTime = prayers.prayerTime[0];
+            
+            for (const [key, value] of Object.entries(prayerTime)) {
+                if (PRAYER_TIMES.includes(key)) {
+                    document.querySelector(`.${key} > h3`).innerHTML = PRAYER_TIMES_TRANSLATE[key];
+                    document.querySelector(`.${key} > div`).innerHTML = value.substring(0,5);
+                }
+                if (key === 'date') {
+                    document.querySelector('.gregorian').innerHTML = formatGregorianDate(value);
+                }
+                if (key === 'hijri') {
+                    document.querySelector('.hijri').innerHTML = formatHijriDate(value);
+                }
+            }
+    }
 }
